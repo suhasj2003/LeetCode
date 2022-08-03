d693b6f45578ca0a386d578b89bdbb7f7d0c8ca7
Suhas Jujjavarapu <suhasj2003@gmail.com>
2022-08-03 02:36:15 -0400

    Time: 2 ms (78.73%), Space: 41.7 MB (28.50%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,25 +1,25 @@
 class Solution {
-    public int reverse(int x) {
-		
-		String num = String.valueOf(x);
-		String result = "";
-		
-		if (num.charAt(0) == '-') {
-			num = num.substring(1, num.length());
-			result += '-';
+     public static int reverse(int x) {
+			
+	        boolean neg = false; 
+	        
+	        if (x < 0) {
+	            neg = true;
+	            x = -x;
+	        }        
+	        
+	        
+	        
+	        long num = 0;
+	        while (x > 0) {
+	            num = 10 * num + x % 10;
+	            x /= 10;
+	        }
+	        
+	        if (num > Integer.MAX_VALUE) {
+	            return 0;
+	        }
+	        
+	        return (int) (neg ? -num : num); 
 		}
-		
-		char[] numChars = new char[num.length()];
-		num.getChars(0, num.length(), numChars, 0);
-		
-		for(int i = numChars.length - 1; i >= 0; i--) {
-			result += numChars[i];
-		}
-		
-		try {
-			return Integer.valueOf(result);
-		} catch (IllegalArgumentException e) {
-			return 0;
-		}
-	}
 }

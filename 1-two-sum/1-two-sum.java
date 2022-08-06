8d8c4d26f8926a608b0e100f5a8f64e416396d2f
Suhas Jujjavarapu <suhasj2003@gmail.com>
2022-08-06 11:54:19 -0400

    Time: 3 ms (91.89%), Space: 45.9 MB (30.93%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,7 +1,7 @@
-import java.util.Hashtable;
+import java.util.HashMap;
 class Solution {
     public static int[] twoSum(int[] nums, int target) {
-		Hashtable<Integer, Integer> a = new Hashtable<Integer, Integer>();
+		HashMap<Integer, Integer> a = new HashMap<Integer, Integer>();
 		for (int i = 0; i < nums.length; i++) {
 			if (a.containsKey(target - nums[i])) {
 				return new int[]{a.get(target - nums[i]), i};

a289fc6cea45ac5f0171b00e48664d30b4987fa5
Suhas Jujjavarapu <suhasj2003@gmail.com>
2022-08-06 18:24:27 -0400

    Time: 22 ms (12.42%), Space: 39.5 MB (94.45%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,11 +1,7 @@
 class Solution {
     public static int mySqrt(int x) {
-		int oddNumber = 1, count = 0;
-		while (x > 0) {
-			x -= oddNumber;
-			count++;
-			oddNumber += 2;
-		}
-		return (x >= 0) ? count : count - 1; 
+		long count = 1;
+		while (count * count <= x) count++;
+		return (int) count - 1; 
 	}
 }

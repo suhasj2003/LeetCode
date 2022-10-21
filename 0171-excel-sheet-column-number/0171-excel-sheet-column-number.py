84e9e346141891bdb3348003a0c9d09d333297b9
Suhas Jujjavarapu <suhasj2003@gmail.com>
2022-10-21 00:10:45 -0400

    Time: 70 ms (21.31%), Space: 13.8 MB (55.15%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         sum = 0
-        for i,n in enumerate(columnTitle):
+        for n in columnTitle:
             sum *= 26
             sum += (ord(n)-64)
         return sum

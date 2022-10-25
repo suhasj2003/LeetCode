8a8e0e09b068958aaaf937af66117d7e5415eb51
Suhas Jujjavarapu <suhasj2003@gmail.com>
2022-10-25 19:57:36 -0400

    Time: 2 ms (96.47%), Space: 39.8 MB (95.00%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String countAndSay(int n) {
+        return cASAux(n - 1, "1");
+    }
+    public String cASAux(int n, String s) {
+        if (n == 0) {
+            return s;
+        }
+        char curr = s.charAt(0);
+        int count = 0;
+        StringBuilder temp = new StringBuilder();
+        for (Character c : s.toCharArray()) {
+            if (curr != c) {
+                temp.append(count);
+                temp.append(curr);
+                curr = c;
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+        // count++;
+        temp.append(count);
+        temp.append(curr);
+        return cASAux(n - 1, temp.toString());
+    }
+}
